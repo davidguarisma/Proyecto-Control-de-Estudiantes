2582e6e989d487921171e0cabe094479e2511ee8
davidguarisma <dguarisma@lccopen.tech>
2016-10-24 19:51:04 -0300

    sesion usuario

--- model/alumnos.php
+++ model/alumnos.php
@@ -1,37 +1,64 @@
 <?php
-function get_materias_alumnos(){
-  $mysqli = crearConexion();
-  $consulta = 'SELECT pnf, trayecto,nombre_semestre,id_semestre,
-  materia,id_user,	case pnf WHEN 1 THEN  "PNF en Electricidad"
-        WHEN 2 THEN  "PNF en Ingeniería de Mantenimiento"
-        WHEN 3 THEN  "PNF en Mecánica"
-        WHEN 4 THEN  "PNF en Informática"
-        WHEN 5 THEN  "PNF en Geociencia"
-        WHEN 6 THEN  "PNF en Sistemas de Calidad y Ambiente"
-      END AS pnf,  case trayecto
-            WHEN 1 THEN  "I"
-            WHEN 2 THEN  "II"
-            WHEN 3 THEN  "III"
-            WHEN 4 THEN  "IV"
-            END AS trayecto,  case nombre_semestre
-            WHEN 1 THEN  "I"
-            WHEN 2 THEN  "II"
-            WHEN 3 THEN  "III"
-            WHEN 4 THEN  "IV"
-            WHEN 5 THEN  "V"
-            WHEN 6 THEN  "VI"
-            WHEN 7 THEN  "VII"
-            WHEN 8 THEN  "VIII"
-          END AS nombre_semestre,
-          case apertura WHEN 0 THEN  "Cerrada"
-              WHEN 1 THEN  "Activa"
-             END AS apertura
-            FROM materias m , semestre s, usuarios u WHERE m.apertura=1 ';
-  $resultado = $mysqli->query($consulta);
+  function get_materias_alumnos(){
+    $mysqli = crearConexion();
+    // $consulta = 'SELECT pnf_user,
+    //   	case pnf_user WHEN 1 THEN  "PNF en Electricidad"
+    //       WHEN 2 THEN  "PNF en Ingeniería de Mantenimiento"
+    //       WHEN 3 THEN  "PNF en Mecánica"
+    //       WHEN 4 THEN  "PNF en Informática"
+    //       WHEN 5 THEN  "PNF en Geociencia"
+    //       WHEN 6 THEN  "PNF en Sistemas de Calidad y Ambiente"
+    //     END AS pnf_user,
+    //     case 	trayecto_user
+    //         WHEN 1 THEN  "I"
+    //         WHEN 2 THEN  "II"
+    //         WHEN 3 THEN  "III"
+    //         WHEN 4 THEN  "IV"
+    //         END AS trayecto_user,
+    //        case semestre_user
+    //         WHEN 1 THEN  "I"
+    //         WHEN 2 THEN  "II"
+    //         WHEN 3 THEN  "III"
+    //         WHEN 4 THEN  "IV"
+    //         WHEN 5 THEN  "V"
+    //         WHEN 6 THEN  "VI"
+    //         WHEN 7 THEN  "VII"
+    //         WHEN 8 THEN  "VIII"
+    //       END AS semestre_user
+    //           FROM usuarios WHERE id_user ="'.$_SESSION['id_register'].'"';
+    $consulta = 'SELECT id_materias, materia, nombre_semestre, trayecto, id_semestre,
+      case pnf WHEN 1 THEN  "PNF en Electricidad"
+          WHEN 2 THEN  "PNF en Ingeniería de Mantenimiento"
+              WHEN 3 THEN  "PNF en Mecánica"
+              WHEN 4 THEN  "PNF en Informática"
+              WHEN 5 THEN  "PNF en Geociencia"
+              WHEN 6 THEN  "PNF en Sistemas de Calidad y Ambiente"
+          END AS pnfs,
+      case apertura WHEN 0 THEN  "Cerrada"
+            WHEN 1 THEN  "Activa"
+          END AS apert,
+          case trayecto
+              WHEN 1 THEN  "I"
+          WHEN 2 THEN  "II"
+              WHEN 3 THEN  "III"
+              WHEN 4 THEN  "IV"
+          END AS tray,
+          case nombre_semestre
+              WHEN 1 THEN  "I"
+          WHEN 2 THEN  "II"
+              WHEN 3 THEN  "III"
+              WHEN 4 THEN  "IV"
+              WHEN 5 THEN  "V"
+              WHEN 6 THEN  "VI"
+              WHEN 7 THEN  "VII"
+              WHEN 8 THEN  "VIII"
+          END AS semestre
+      FROM materias INNER JOIN semestre on semestre_id = id_semestre AND nombre_semestre = "'. $_SESSION['semestre_user'].'"';
+    $resultado = $mysqli->query($consulta);
 
-  $data = array();
-  while ( $row = $resultado->fetch_array()) {
-  $data[] =$row;
-  }
-  return $data;
+    $data = array();
+    while ( $row = $resultado->fetch_array()) {
+    $data[] =$row;
+    }
+    return $data;
 }
