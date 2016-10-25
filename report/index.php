cc50e6222e347ede00682541c5129085e212b81a
Brito <josebrito0623@gmail.com>
2016-10-24 21:21:21 -0400

    Module Report

--- report/index.php
+++ report/index.php
@@ -1,35 +1,125 @@
 <?php
 	session_start();
-
-		$conexion = mysql_connect("localhost","root","root");
-		mysql_select_db("iuteb",$conexion);
-
-		date_default_timezone_set("America/Caracas");
-
-		function limpiar($tags){
-			$tags = strip_tags($tags);
-			$tags = stripslashes($tags);
-			$tags = htmlentities($tags);
-			$tags = mysql_real_escape_string($tags);
-			return $tags;
-		}
-
-	include_once('Class/funciones.php');
-	include_once('Class/class_alumnos.php');
-
-
 	$id_curso=$_GET['curso'];
-	if($_POST['curso']<>'X'){
-	$obj_curso=new reporte($id_curso);
-		$nombre_curso=$obj_curso->consultar('nombre');
-		$descri_curso=$obj_curso->consultar('curso');
-	}else{
-		$nombre_curso='Todos';
-		$descri_curso='';
+	$tr=$_GET['tra'];
+	$se=$_GET['sem'];
+	$pnfs=$_GET['pnf'];
+
+
+	switch ($pnfs) {
+		case 1:
+			$pnf= 'PNF en Electricidad';
+			break;
+		case 2:
+			$pnf= 'PNF en Ingeniería de Mantenimiento';
+			break;
+		case 3:
+			$pnf= 'PNF en Mecánica';
+			break;
+		case 4:
+			$pnf= 'PNF en Informática';
+			break;
+		case 5:
+			$pnf= 'PNF en Geociencia';
+			break;
+		case 6:
+			$pnf= 'PNF en Sistemas de Calidad y Ambiente';
+			break;
+		default:
+			# code...
+			break;
 	}
-	header("Content-Disposition: attachment; filename=Report_".$nombre_curso.".xls");
-	header("Pragma: no-cache");
+
+switch ($se) {
+	case 1:
+		$sem = 'Semestre I';
+		break;
+	case 2:
+		$sem = 'Semestre II';
+		break;
+	case 3:
+		$sem = 'Semestre III';
+		break;
+	case 4:
+		$sem = 'Semestre IV';
+		break;
+	case 5:
+		$sem = 'Semestre V';
+		break;
+	case 6:
+		$sem = 'Semestre VI';
+		break;
+	case 7:
+		$sem = 'Semestre VII';
+		break;
+	case 8:
+		$sem = 'Semestre VIII';
+		break;
+	default:
+		
+		break;
+}
+
+switch ($tr) {
+	case 1:
+		$tra = 'trayecto I';
+		break;
+	case 2:
+		$tra = 'trayecto II';
+		break;
+	case 3:
+		$tra = 'trayecto III';
+		break;
+	case 4:
+		$tra = 'trayecto IV';
+		break;
+	default:
+		
+		break;
+}
+
+
+	date_default_timezone_set("America/Caracas");
+
+
+	include('../config/config.php');
+	
+	$mysqli = crearConexion();
+    
+$consultaM = '
+		SELECT materia, semestre_id FROM materias WHERE id_materias = "'.$id_curso.'" ';
+
+	$resultadoM = $mysqli->query($consultaM);
+	
+	$datosM = array();
+	
+	while ($rowM = $resultadoM->fetch_array()){
+    		$nombreMateria = $rowM['materia'];
+    		$idSemestre = $rowM['semestre_id'];
+	}
+
+
+    $consulta = '
+		SELECT nombres, apellidos, cedula FROM usuarios INNER JOIN inscripcion on id_semestre = "'.$idSemestre.'" 
+			GROUP BY id_user';
+
+	$resultado = $mysqli->query($consulta);
+	
+	$datos = array();
+	
+	while ($row = $resultado->fetch_array()){
+    		$datos[] = $row;
+	}
+
+
+		$nombre_curso= $nombreMateria;
+	// 	$descri_curso='otro';
+
+	 header("Content-Disposition: attachment; filename=Report_".$nombre_curso.".xls");
+	 header("Pragma: no-cache");
+
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -40,35 +130,47 @@
 <body>
 <table width="100%" border="1">
   <tr>
-    <td colspan="7">
-    	<center><strong><?php echo $nombre_curso.' | '.$descri_curso; ?></strong></center>
+    <td colspan="6">
+    	<center><strong><?php echo $nombre_curso; ?></strong></center>
     </td>
   </tr>
   <tr>
     <td width="5%"><center><strong>No.</strong></center></td>
-    <td width="29%"><strong>Apellido y Nombre del Alumno</strong></td>
-    <td width="19%"><center><strong>Cedula</strong></center></td>
+    <td width="20%"><center><strong>PNF</strong></center></td>    
+    <td width="20%"><center><strong>Trayecto</strong></center></td>    
+    <td width="20%"><center><strong>Semestre</strong></center></td>    
+    <td width="20%"><strong>Apellido y Nombre del Alumno</strong></td>
+    <td width="15%"><center><strong>Cedula</strong></center></td>
 
   </tr>
-  <?php
-  	$n=0;
-  	if($_POST['curso']<>'X'){
-  		$sql="SELECT * FROM alumnos WHERE materia='$id_curso' and estado='s'";
-	}else{
-		$sql="SELECT * FROM alumnos WHERE estado='s'";
-	}
-	$can=mysql_query($sql);
-	while($dato=mysql_fetch_array($can)){
-		$n++;
-		$salones = new reporte($dato['materia']);
-  ?>
+
+<?php 
+      $number =0;
+   if (is_array($datos) || is_object($datos)){
+     foreach ($datos as $item) {
+      $number++;
+
+     echo '  
   <tr>
-    <td><center><strong><?php echo $n; ?></strong></center></td>
-    <td><?php echo trim($dato['apellido']).' '.trim($dato['nombre']); ?></td>
-    <td><center><?php echo $dato['cedula']; ?></center></td>
+    <td><center><strong>'.$number.'</strong></center></td>
+    <td><center>'.$pnf.'</center></td>    
+    <td><center>'.$tra.'</center></td>    
+    <td><center>'.$sem.'</center></td>    
+    <td>'.$item["nombres"].' '.$item["apellidos"].'</td>
+    <td><center>'.$item["cedula"].'</center></td>
+  </tr>';
 
-  </tr>
-  <?php } ?>
+  }
+   }
+if($number == 0){
+   echo '  
+  <tr>
+    <td colspan="6"><center>No hay regitros </center></td>    
+  </tr>';
+
+}   
+
+ ?>
 </table>
 </body>
 </html>
